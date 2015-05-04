f121757a3661602bbd5cda0b3b4b2b5685f8ce8f
Daniel Bimschas <daniel@bimschas.com>
2015-05-04 18:11:52 +0200

    ...

---
+++ closures.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func genAdd(howMuch int) func(int) int {
+	return func(a int) int {
+		return a + howMuch
+	}
+}
+
+func main() {
+	addOne := genAdd(1)
+	addTwo := genAdd(2)
+	addThree := genAdd(3)
+	
+	fmt.Println(addOne(1))
+	fmt.Println(addTwo(1))
+	fmt.Println(addThree(1))
+}
